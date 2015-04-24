28b0cc247cd6f1b72d916022d2d840e19f64b9f6
Rodrigo Morgado <rodrigomorgado28@gmail.com>
2015-04-24 20:31:05 -0300

    Colocando a tabela de ranking mais bonita

--- AppAngular/Ranking/ranking.service.js
+++ AppAngular/Ranking/ranking.service.js
@@ -17,18 +17,27 @@
         // implementacao das funcoes do servico
         function getRanking() {
 
+            var mockRanking = [
+            { name: 'Candidato 1', score: 40 },
+            { name: 'Candidato 2', score: 35 },
+            { name: 'Candidato 3', score: 55 },
+            { name: 'Candidato 4', score: 37 }
+            ];
+
             var deferred = $q.defer();
 
             
-            $http.get('/api/getRanking', {}).success(successCallback).error(errorCallback);
+            //$http.get('/api/getRanking', {}).success(successCallback).error(errorCallback);
 
-            function successCallback(data) {
-                deferred.resolve(data);
-            };
+            //function successCallback(data) {
+            //    deferred.resolve(data);
+            //};
 
-            function errorCallback(error) {
-                deferred.reject(error);
-            };
+            //function errorCallback(error) {
+            //    deferred.reject(error);
+            //};
+
+            deferred.resolve(mockRanking);
 
             return deferred.promise;
 
