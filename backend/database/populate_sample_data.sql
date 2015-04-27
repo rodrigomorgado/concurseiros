60a543da51ac7ecbf4578652ea61349679fc324c
eduardo sobral <easobral@gmail.com>
2015-04-27 10:58:22 -0300

    added script to populate with sampledata

---
+++ backend/database/populate_sample_data.sql
@@ -0,0 +1,11 @@
+INSERT INTO users ('jhon doe 1', 'sample1@sample.com');
+INSERT INTO users ('jhon doe 2', 'sample2@sample.com');
+INSERT INTO users ('jhon doe 3', 'sample3@sample.com');
+INSERT INTO users ('jhon doe 4', 'sample4@sample.com');
+INSERT INTO users ('jhon doe 5', 'sample5@sample.com');
+
+INSERT INTO scores(1, 10.0);
+INSERT INTO scores(2, 9.0);
+INSERT INTO scores(3, 9.5);
+INSERT INTO scores(4, 8.0);
+INSERT INTO scores(5, 8.5);
