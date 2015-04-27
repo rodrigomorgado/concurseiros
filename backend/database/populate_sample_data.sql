e25aefef4b2cdd48c039266978d6f42049650c27
eduardo sobral <easobral@gmail.com>
2015-04-27 11:50:29 -0300

    corrigido erro com a sintaxe

--- backend/database/populate_sample_data.sql
+++ backend/database/populate_sample_data.sql
@@ -1,5 +1,5 @@
-INSERT INTO users ('jhon doe 1', 'sample1@sample.com', 10.0);
-INSERT INTO users ('jhon doe 2', 'sample2@sample.com', 9.0);
-INSERT INTO users ('jhon doe 3', 'sample3@sample.com', 9.5);
-INSERT INTO users ('jhon doe 4', 'sample4@sample.com', 8.0);
-INSERT INTO users ('jhon doe 5', 'sample5@sample.com', 8.5);
+INSERT INTO users VALUES ('jhon doe 1', 'sample1@sample.com', 10.0);
+INSERT INTO users VALUES ('jhon doe 2', 'sample2@sample.com', 9.0);
+INSERT INTO users VALUES ('jhon doe 3', 'sample3@sample.com', 9.5);
+INSERT INTO users VALUES ('jhon doe 4', 'sample4@sample.com', 8.0);
+INSERT INTO users VALUES ('jhon doe 5', 'sample5@sample.com', 8.5);
