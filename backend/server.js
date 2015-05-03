6da2815314cd268a59168af13bd5219fd05f049a
eduardo sobral <easobral@gmail.com>
2015-05-03 14:42:14 -0300

    refactor do servidor para servir arquivos staticos de forma mais simples

--- backend/server.js
+++ backend/server.js
@@ -13,54 +13,8 @@ var app = express();
 
 app.use(bodyParse.json());
 
-app.get('/', function (req, res) {
-    'use strict';
-    var reqUrl = url.parse(req.url, true);
-    res.writeHead(200, { 'content-type': 'text/html' });
-    fs.createReadStream(cconf.web + sconf.default_file).pipe(res);
-});
-
-app.get('/favicon.ico', function (req, res) {
-    'use strict';
-    res.writeHead(200, { 'content-type': 'image/x-icon' });
-    fs.createReadStream(cconf.web + 'images' + sconf.default_file).pipe(res);
-});
-
-
-app.get('/scripts/*', function (req, res) {
-    'use strict';
-    var reqUrl = url.parse(req.url, true);
-    res.writeHead(200, { 'content-type': 'text/javascript' });
-    fs.createReadStream(cconf.web + reqUrl.pathname).pipe(res);
-});
-
-app.get('/styles/*', function (req, res) {
-    'use strict';
-    var reqUrl = url.parse(req.url, true);
-    res.writeHead(200, { 'content-type': 'text/css' });
-    fs.createReadStream(cconf.web + reqUrl.pathname).pipe(res);
-});
-
-app.get('/fonts/*', function (req, res) {
-    'use strict';
-    var reqUrl = url.parse(req.url, true);
-    fs.createReadStream(cconf.web + reqUrl.pathname).pipe(res);
-});
-
-app.get('/images/*', function (req, res) {
-    'use strict';
-    var reqUrl = url.parse(req.url, true);
-    res.writeHead(200);
-    fs.createReadStream(cconf.web + reqUrl.pathname).pipe(res);
-});
-
-app.get('/AppAngular/*', function (req, res) {
-    'use strict';
-    var reqUrl = url.parse(req.url, true);
-    res.writeHead(200, { 'content-type': 'text/javascript' });
-    fs.createReadStream(cconf.web + reqUrl.pathname).pipe(res);
-});
-
+app.use(express.static(cconf.web));
+    
 app.get(endpoint.getRanking, function (req, res) {
     'use strict';
     var connection = mysql.createConnection(sconf.mysql);
@@ -89,18 +43,6 @@ app.post(endpoint.insertScore, function (req, res) {
             //Query failed. Send a status code 500
             res.status(500);
             res.end(err);
-        }
-    });
-});
-
-app.get(endpoint.updateServer, function (req, res) {
-    'use strict';
-    exec(cconf.backend + '/scripts/update_server.sh', function (err, stdout, stderr) {
-        if (!err) {
-            res.end(stdout);
-        } else {
-            res.send(err);
-            res.end(stderr);
         }
     });
 });
