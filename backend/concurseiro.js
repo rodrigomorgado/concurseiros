f4451438f7a2f90c4689ace7d7c4b9c26e332001
eduardo sobral <easobral@gmail.com>
2015-06-22 14:53:44 -0300

    added Create e Recover do TRT

--- backend/concurseiro.js
+++ backend/concurseiro.js
@@ -14,7 +14,8 @@ var app = express();
 app.use(bodyParse.json());
 
 app.use(express.static(cconf.web));
-    
+
+//RECOVER MARINHA
 app.get(endpoint.getRanking, function (req, res) {
     'use strict';
     var connection = mysql.createConnection(sconf.mysql);
@@ -31,6 +32,7 @@ app.get(endpoint.getRanking, function (req, res) {
 	});
 });
 
+//CREATE MARINHA
 app.post(endpoint.insertScore, function (req, res) {
     'use strict';
     var connection = mysql.createConnection(sconf.mysql);
@@ -46,5 +48,40 @@ app.post(endpoint.insertScore, function (req, res) {
         }
     });
 });
+
+//RECOVER TRT
+app.get(endpoint.getRankingTRT, function (req, res) {
+    'use strict';
+    var connection = mysql.createConnection(sconf.mysql);
+    connection.query('SELECT id, name, email, score FROM users_trt', function (err, rows, fields) {
+        connection.end();
+        if (!err) {
+            //Returns the rows to the user with a status code 200
+            res.status(200).send(JSON.stringify(rows));
+        } else {
+            //Query failed. Send a status code 500
+            res.status(500);
+            res.end(err);
+        }
+	});
+});
+
+//CREATE TRT
+app.post(endpoint.insertScoreTRT, function (req, res) {
+    'use strict';
+    var connection = mysql.createConnection(sconf.mysql);
+    connection.query('INSERT INTO users_trt SET ?', req.body, function (err) {
+        connection.end();
+        if (!err) {
+            //Returns nothing to the user with a status code 200
+            res.status(200).end('ok');
+        } else {
+            //Query failed. Send a status code 500
+            res.status(500);
+            res.end(err);
+        }
+    });
+});
+
 
 app.listen(sconf.port);
